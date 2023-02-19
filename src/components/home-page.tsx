575049cf7c8db27df017a68f166a64db1ab02a6b
t.grigorov <t.grigorov99@gmail.com>
2023-02-19 18:47:54 +0100

    feat: styles to heading

--- src/components/home-page.tsx
+++ src/components/home-page.tsx
@@ -4,7 +4,7 @@ import styles from '@/styles/Home.module.scss';
 const HomePage: React.FC = (): JSX.Element => {
   return (
     <div className={styles.main}>
-      <h1>Start your dream trip today!</h1>
+      <h1 className={styles.home__heading}>Start your dream trip today!</h1>
     </div>
   );
 };
