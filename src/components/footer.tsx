27341aabe026305806749e395d65803a8b32c93f
t.grigorov <t.grigorov99@gmail.com>
2023-02-19 15:43:51 +0100

    feat: footer component with styles

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import styles from '@/styles/Footer.module.scss';
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p> © 2023 Transport Planner - Todor Grigorov </p>
+    </footer>
+  );
+};
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-// import { Footer } from './footer';
+import { Footer } from './footer';
 import { Header } from './header';
 
 type Props = {
@@ -11,7 +11,7 @@ const MainLayout: React.FC<Props> = ({ children }: Props): JSX.Element => {
     <>
       <Header />
       <main>{children}</main>
-      {/*<Footer />*/}
+      <Footer />
     </>
   );
 };
