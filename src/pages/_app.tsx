90f2d7e3bb04f74db15c072f63077ef9a4e309cc
t.grigorov <t.grigorov99@gmail.com>
2023-02-20 22:47:15 +0100

    feat: add SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,26 @@
 import MainLayout from '@/components/layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { DefaultSeo } from 'next-seo';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <DefaultSeo
+        title="Next SEO Example"
+        description="Next SEO is a plug in that makes managing your SEO easier in Next.js projects."
+        openGraph={{
+          type: 'website',
+          locale: 'en_IE',
+          url: 'https://www.url.ie/',
+          siteName: 'SiteName',
+        }}
+        twitter={{
+          handle: '@handle',
+          site: '@site',
+          cardType: 'summary_large_image',
+        }}
+      />
       <MainLayout>
         <Component {...pageProps} />
       </MainLayout>
